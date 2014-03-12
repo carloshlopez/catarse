b0998dab74d8f5d9b8d0143c5ec4528f706a19f7
Carlos Lopez <carloshlopez@gmail.com>
2014-03-12 18:40:24 -0500

    updated versions of pagosonline

--- vendor/cache/catarse_pagosonline-c4df449d16a9/app/controllers/catarse_pagosonline/payment/pagosonline_controller.rb
+++ vendor/cache/catarse_pagosonline-c4df449d16a9/app/controllers/catarse_pagosonline/payment/pagosonline_controller.rb
@@ -33,7 +33,8 @@ module CatarsePagosonline::Payment
       #contribution = current_user.backs.find params[:id]
       contribution = ::Contribution.find(params[:id])
       begin
-        response = @@gateway.Response.new(params)
+        # response = @@gateway.Response.new(params)
+        response = Pagosonline::Response.new(@@gateway, params)
         if response.valid?
           contribution.update_attribute :payment_method, 'PagosOnline'
           contribution.update_attribute :payment_token, response.transaccion_id
@@ -47,8 +48,8 @@ module CatarsePagosonline::Payment
           return redirect_to main_app.new_project_contribution_path(contribution.project)  
         end
       rescue Exception => e
-        ::Airbrake.notify({ :error_class => "PagosOnline Error", :error_message => "PagosOnline Error: #{e.inspect}", :parameters => params}) rescue nil
-        Rails.logger.info "-----> #{e.inspect}"
+        # ::Airbrake.notify({ :error_class => "PagosOnline Error", :error_message => "PagosOnline Error: #{e.inspect}", :parameters => params}) rescue nil
+        Rails.logger.info "--success error-----> #{e.inspect}"
         pagosonline_flash_error
         return redirect_to main_app.new_project_contribution_path(contribution.project)
       end
@@ -57,7 +58,8 @@ module CatarsePagosonline::Payment
     def notifications
       # contribution = current_user.backs.find params[:id]
       contribution = ::Contribution.find(params[:id])
-      response = @@gateway.Response.new(params)
+      response = Pagosonline::Response.new(@@gateway, params)
+      # response = @@gateway.Response.new(params)
       if response.valid?
         proccess!(contribution, response)
         render status: 200, nothing: true
@@ -65,8 +67,8 @@ module CatarsePagosonline::Payment
         render status: 404, nothing: true
       end
     rescue Exception => e
-      ::Airbrake.notify({ :error_class => "PagosOnline Notification Error", :error_message => "PagosOnline Notification Error: #{e.inspect}", :parameters => params}) rescue nil
-      Rails.logger.info "-----> #{e.inspect}"
+      # ::Airbrake.notify({ :error_class => "PagosOnline Notification Error", :error_message => "PagosOnline Notification Error: #{e.inspect}", :parameters => params}) rescue nil
+      Rails.logger.info "--notifications error-----> #{e.inspect}"
       render status: 404, nothing: true
     end
 
@@ -93,16 +95,14 @@ module CatarsePagosonline::Payment
     end
 
     def setup_gateway
-      if ::Configuration[:pagosonline_username] and ::Configuration[:pagosonline_key] and ::Configuration[:pagosonline_merchant_id] and ::Configuration[:pagosonline_account_id]
+      if ::Configuration[:pagosonline_key] and ::Configuration[:pagosonline_account_id]
         @@gateway ||= Pagosonline::Client.new({
-          merchant_id: ::Configuration[:pagosonline_merchant_id],
           account_id: ::Configuration[:pagosonline_account_id],
-          login: ::Configuration[:pagosonline_username],
           key: ::Configuration[:pagosonline_key],
           test: true
         })
       else
-        raise "[PagosOnline] pagosonline_username, pagosonline_key, pagosonline_merchant_id and pagosonline_account_id are required to make requests to PagosOnline"
+        raise "[PagosOnline] pagosonline_key and pagosonline_account_id are required to make requests to PagosOnline"
       end
     end
 
--- vendor/cache/catarse_pagosonline-c4df449d16a9/catarse_pagosonline.gemspec
+++ vendor/cache/catarse_pagosonline-c4df449d16a9/catarse_pagosonline.gemspec
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
-# stub: catarse_pagosonline 0.1.10 ruby lib
+# stub: catarse_pagosonline 0.1.11 ruby lib
 
 Gem::Specification.new do |s|
   s.name = "catarse_pagosonline"
-  s.version = "0.1.10"
+  s.version = "0.1.11"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.require_paths = ["lib"]
