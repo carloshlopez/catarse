212d6b92da9a03a5cf0925bcc80920e5fbbafa9c
Carlos López <carloshlopez@gmail.com>
2015-04-15 17:12:38 -0500

    new pagosonline

--- vendor/cache/catarse_pagosonline-2b817e16105f/config/routes.rb
+++
@@ -1,9 +0,0 @@
-CatarsePagosonline::Engine.routes.draw do
-  namespace :payment do
-    get '/pagosonline/:id/review' => 'pagosonline#review', :as => 'review_pagosonline'
-    post '/pagosonline/notifications' => 'pagosonline#ipn',  :as => 'ipn_pagosonline'
-    post '/pagosonline/:id/notifications' => 'pagosonline#notifications',  :as => 'notifications_pagosonline'
-    get '/pagosonline/:id/success'       => 'pagosonline#success',        :as => 'success_pagosonline'
-    post '/pagosonline/:id/cancel'        => 'pagosonline#cancel',         :as => 'cancel_pagosonline'
-  end
-end
