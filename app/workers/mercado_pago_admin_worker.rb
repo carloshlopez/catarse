1812e5eef9bbbb95a8e45a8073ffd25932d0c33f
Carlos Lopez(mac-mini) <carloshlopez@gmail.com>
2015-12-02 15:35:21 -0500

    mp email

--- app/workers/mercado_pago_admin_worker.rb
+++ app/workers/mercado_pago_admin_worker.rb
@@ -3,10 +3,15 @@ class MercadoPagoAdminWorker
   sidekiq_options retry: 5
 
   def perform mercado_pago_client_id
-  	puts "Estamos en perform con #{mercado_pago_client_id}"
-  	mercado_pago_client = MercadoPagoClient.find mercado_pago_client_id
-    user = User.find_by_email(mercado_pago_client.email)
-    mp_url = "#{MERCADOPAGO_AUTH_URL}#{Rails.application.routes.url_helpers.mercado_pago_clients_code_url}?project_id=#{mercado_pago_client.project_id}"
-    mercado_pago_client.notify_project_user(user, mp_url) 
+  	begin
+	  	puts "Estamos en perform con #{mercado_pago_client_id}"
+	  	mercado_pago_client = MercadoPagoClient.find mercado_pago_client_id
+	    user = User.find_by_email(mercado_pago_client.email)
+	    mp_url = "#{MERCADOPAGO_AUTH_URL}#{Rails.application.routes.url_helpers.mercado_pago_clients_code_url}?project_id=#{mercado_pago_client.project_id}"
+	    mercado_pago_client.notify_project_user(user, mp_url) 
+  	rescue Exception => e
+  		puts "Error en MercadoPagoAdminWorker #{e.message}"
+  	end
+
   end
 end
